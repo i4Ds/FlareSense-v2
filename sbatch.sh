d400afabee11ad80b7c139e62f828449fdc03ae8
Vincenzo Timmel <vincenzo.timmel@fhnw.ch>
2024-03-06 20:07:17 +0100

    Added new config, reduced epochs.

--- sbatch.sh
+++ sbatch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --time=8:00:00
+#SBATCH --time=06:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name flaresense-v2
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --partition=top6
-#SBATCH --out=logs/stt_eval_%j.out
-#SBATCH --error=logs/stt_eval_%j.err
+#SBATCH --out=logs/%j.out
+#SBATCH --error=logs/%j.err
 
-python3 main.py
+srun python3 main.py --config configs/t200.yml
