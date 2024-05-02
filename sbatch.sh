cc013d4b5fad034ac26791e1914b39eb79385f88
Vincenzo Timmel <vincenzo.timmel@fhnw.ch>
2024-05-02 19:18:19 +0200

    Updated sweep parameters.

--- sbatch.sh
+++ sbatch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --time=16:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name flaresense-v2
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH --partition=performance
+#SBATCH --partition=p4500
 #SBATCH --out=logs/%j.out
 #SBATCH --error=logs/%j.err
 
-wandb agent vincenzo-timmel/FlareSense-v2/1dpxgbtd
+wandb agent vincenzo-timmel/FlareSense-v2/o52dwod0
